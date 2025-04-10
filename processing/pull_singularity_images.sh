5482e6a895e4c90d9a8ff18c831b98e141be7c97
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-04-10 14:46:41 -0400

    Update.

---
+++ processing/pull_singularity_images.sh
@@ -0,0 +1,2 @@
+singularity pull --name /cbica/projects/nibs/apptainer/qsiprep-1.0.0.sif docker://pennlinc/qsiprep:1.0.0
+singularity pull --name /cbica/projects/nibs/apptainer/qsirecon-1.0.1.sif docker://pennlinc/qsirecon:1.0.1
