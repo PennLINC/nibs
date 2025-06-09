844522c9e62f2b3f3b45262a2dd122030c17e2f3
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-06-09 11:35:59 -0400

    Work on MESE processing script.

--- processing/process_mese.py
+++ processing/process_mese.py
@@ -7,51 +7,54 @@ Plus we need proper output names.
 
 import json
 import os
+import shutil
 
-import nibabel as nb
-from ants import apply_transforms, image_read, image_write, registration
+import ants
 from bids.layout import BIDSLayout, Query
-from nilearn import image
-from pymp2rage import MP2RAGE
+from sdcflows.workflows.fit.pepolar import init_topup_wf
+
+from utils import get_filename
 
 
 def collect_run_data(layout, bids_filters):
     queries = {
-        'inv1_magnitude': {
+        'mese_mag_ap': {
             'part': ['mag', Query.NONE],
-            'inv': 1,
-            'suffix': 'MP2RAGE',
+            'echo': 1,
+            'direction': 'AP',
+            'suffix': 'MESE',
             'extension': ['.nii', '.nii.gz'],
         },
-        'inv1_phase': {
-            'part': 'phase',
-            'inv': 1,
-            'suffix': 'MP2RAGE',
-            'extension': ['.nii', '.nii.gz'],
-        },
-        'inv2_magnitude': {
+        'mese_mag_pa': {
             'part': ['mag', Query.NONE],
-            'inv': 2,
-            'suffix': 'MP2RAGE',
+            'echo': 1,
+            'direction': 'PA',
+            'suffix': 'MESE',
             'extension': ['.nii', '.nii.gz'],
         },
-        'inv2_phase': {
-            'part': 'phase',
-            'inv': 2,
-            'suffix': 'MP2RAGE',
+        't1w': {
+            'datatype': 'anat',
+            'space': Query.NONE,
+            'resolution': Query.NONE,
+            'desc': 'preproc',
+            'suffix': 'T1w',
             'extension': ['.nii', '.nii.gz'],
         },
-        'b1_famp': {
-            'datatype': 'fmap',
-            'acquisition': 'famp',
-            'suffix': 'TB1TFL',
+        't1w_mask': {
+            'datatype': 'anat',
+            'space': Query.NONE,
+            'resolution': Query.NONE,
+            'desc': 'brain',
+            'suffix': 'mask',
             'extension': ['.nii', '.nii.gz'],
         },
-        'b1_anat': {
-            'datatype': 'fmap',
-            'acquisition': 'anat',
-            'suffix': 'TB1TFL',
-            'extension': ['.nii', '.nii.gz'],
+        't1w2mni_xfm': {
+            'datatype': 'anat',
+            'from': 'T1w',
+            'to': 'MNI152NLin2009cAsym',
+            'mode': 'image',
+            'suffix': 'xfm',
+            'extension': '.h5',
         },
     }
 
@@ -99,100 +102,206 @@ def fit_monoexponential(in_files, echo_times):
 
 
 def process_run(layout, run_data, out_dir, temp_dir):
-    inv1_metadata = layout.get_metadata(run_data['inv1_magnitude'])
-    inv2_metadata = layout.get_metadata(run_data['inv2_magnitude'])
-    b1map_metadata = layout.get_metadata(run_data['b1_famp'])
+    """Process a single run of MESE data.
 
-    # Rescale b1_famp to percentage of flip angle
-    scalar = b1map_metadata['FlipAngle'] * 10
-    # scalar = 90 * 10  # original scalar from Manuel, but I think he had the wrong FA
-    b1map_rescaled = image.math_img(f'img / {scalar}', img=run_data['b1_famp'])
-    b1map_rescaled_path = os.path.join(temp_dir, os.path.basename(run_data['b1_famp']))
-    b1map_rescaled.to_filename(b1map_rescaled_path)
+    Parameters
+    ----------
+    layout : BIDSLayout
+        BIDSLayout object for the dataset.
+    run_data : dict
+        Dictionary containing the paths to the MESE data.
+    out_dir : str
+        Path to the output directory.
+    temp_dir : str
+        Path to the temporary directory.
 
-    # Register b1_famp to inv1_magnitude using b1_anat with ANTs
-    fixed_img = image_read(run_data['inv1_magnitude'])
-    moving_img = image_read(run_data['b1_anat'])
-    reg_output = registration(
+    Notes
+    -----
+    Steps:
+
+    1. Calculate T2* map from AP MESE data.
+    2. Calculate distortion map from AP and PA echo-1 data.
+    3. Perform SDC on T2* map.
+    4. Perform SDC on AP echo-1 data.
+    5. Coregister AP echo-1 data to preprocessed T1w.
+    6. Write out transform to preprocessed T1w.
+    7. Apply transforms to MNI152NLin2009cAsym
+       (distortion map, coregistration transform, normalization transform from sMRIPrep).
+    """
+    name_source = run_data['mese_mag_ap'][0]
+    mese_ap_metadata = [layout.get_metadata(f) for f in run_data['mese_mag_ap']]
+    mese_pa_metadata = layout.get_metadata(run_data['mese_mag_pa'])
+    echo_times = [m['EchoTime'] * 1000 for m in mese_ap_metadata]
+    t2s_img, s0_img = fit_monoexponential(
+        in_files=run_data['mese_mag_ap'],
+        echo_times=echo_times,
+    )
+    t2s_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'anat',
+            'suffix': 'T2star',
+            'extension': '.nii.gz',
+        },
+        dismiss_entities=['echo', 'part'],
+    )
+    t2s_img.to_filename(t2s_filename)
+
+    s0_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'anat',
+            'suffix': 'S0map',
+            'extension': '.nii.gz',
+        },
+        dismiss_entities=['echo', 'part'],
+    )
+    s0_img.to_filename(s0_filename)
+
+    # Calculate distortion map from AP and PA echo-1 data
+    mese_mag_ap_echo1 = run_data['mese_mag_ap'][0]
+    mese_mag_pa_echo1 = run_data['mese_mag_pa'][0]
+    topup_wf = init_topup_wf(
+        grid_reference=0,
+        use_metadata_estimates=False,
+        fallback_total_readout_time=None,
+        omp_nthreads=1,
+        sloppy=False,
+        debug=False,
+        name='topup_estimate_wf',
+    )
+    topup_wf.inputs.in_files = [mese_mag_ap_echo1, mese_mag_pa_echo1]
+    topup_wf.inputs.metadata = [mese_ap_metadata[0], mese_pa_metadata]
+    wf_results = topup_wf.run()
+    # output fields are fmap, fmap_ref, fmap_coeff, fmap_mask, jacobians, xfms, out_warps, method
+    fmap_coeff = wf_results.outputs.fmap_coeff
+    fmap_ref = wf_results.outputs.fmap_ref
+    fmap = wf_results.outputs.fmap
+    fmap_mask = wf_results.outputs.fmap_mask
+    # jacobians = wf_results.outputs.jacobians
+    # xfms = wf_results.outputs.xfms
+    # out_warps = wf_results.outputs.out_warps
+
+    fmap_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'fmap',
+            'fmapid': 'MESE',
+            'desc': 'preproc',
+            'suffix': 'fieldmap',
+            'extension': '.nii.gz',
+        },
+        dismiss_entities=['echo', 'part'],
+    )
+    shutil.copyfile(fmap, fmap_filename)
+
+    fmap_ref_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'fmap',
+            'fmapid': 'MESE',
+            'desc': 'ref',
+            'suffix': 'fieldmap',
+            'extension': '.nii.gz',
+        },
+        dismiss_entities=['echo', 'part'],
+    )
+    shutil.copyfile(fmap_ref, fmap_ref_filename)
+
+    fmap_coeff_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'fmap',
+            'fmapid': 'MESE',
+            'desc': 'coeff',
+            'suffix': 'fieldmap',
+            'extension': '.nii.gz',
+        },
+        dismiss_entities=['echo', 'part'],
+    )
+    shutil.copyfile(fmap_coeff, fmap_coeff_filename)
+
+    fmap_mask_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'fmap',
+            'fmapid': 'MESE',
+            'desc': 'fieldmap',
+            'suffix': 'mask',
+            'extension': '.nii.gz',
+        },
+        dismiss_entities=['echo', 'part'],
+    )
+    shutil.copyfile(fmap_mask, fmap_mask_filename)
+
+    # Coregister AP echo-1 data to preprocessed T1w
+    fixed_img = ants.image_read(run_data['t1w'])
+    moving_img = ants.image_read(run_data['mese_mag_ap'][0])
+    reg_output = ants.registration(
         fixed=fixed_img,
         moving=moving_img,
         type_of_transform='Rigid',
     )
-    fwd_transform = reg_output['fwdtransforms']
-    b1map_rescaled_img = image_read(b1map_rescaled_path)
-    b1map_rescaled_reg = apply_transforms(
-        fixed=fixed_img,
-        moving=b1map_rescaled_img,
-        transformlist=fwd_transform,
-        interpolator='gaussian',
-    )
-    b1map_rescaled_reg_path = os.path.join(out_dir, os.path.basename(run_data['b1_famp']))
-    image_write(b1map_rescaled_reg, b1map_rescaled_reg_path)
+    if len(reg_output['fwdtransforms']) != 1:
+        print(
+            f'Expected 1 transform, got {len(reg_output["fwdtransforms"])}: '
+            f'{reg_output["fwdtransforms"]}'
+        )
+    fwd_transform = reg_output['fwdtransforms'][0]
 
-    inversion_times = [
-        inv1_metadata['InversionTime'],
-        inv2_metadata['InversionTime'],
-    ]
-    flip_angles = [
-        inv1_metadata['FlipAngle'],
-        inv2_metadata['FlipAngle'],
-    ]
-    repetition_times = [
-        inv1_metadata['RepetitionTimeExcitation'],
-        inv2_metadata['RepetitionTimeExcitation'],
-    ]
-    n_slices = inv1_metadata['NumberShots']
-
-    mp2rage = MP2RAGE(
-        MPRAGE_tr=inv1_metadata['RepetitionTimePreparation'],
-        invtimesAB=inversion_times,
-        flipangleABdegree=flip_angles,
-        B0=inv1_metadata['MagneticFieldStrength'],
-        nZslices=n_slices,
-        FLASH_tr=repetition_times,
-        inv1=run_data['inv1_magnitude'],
-        inv2=run_data['inv2_magnitude'],
-        inv1ph=run_data['inv1_phase'],
-        inv2ph=run_data['inv2_phase'],
+    fwd_transform_filename = get_filename(
+        name_source=name_source,
+        layout=layout,
+        out_dir=out_dir,
+        entities={
+            'datatype': 'anat',
+            'from': 'MESE',
+            'to': 'T1w',
+            'mode': 'image',
+            'suffix': 'xfm',
+            'extension': '.h5',
+        },
+        dismiss_entities=['echo', 'part'],
     )
-    t1map = mp2rage.t1map
-    t1map_arr = t1map.get_fdata()
-    t1map_arr = t1map_arr / 1000  # Convert from milliseconds to seconds
-    t1map = nb.Nifti1Image(t1map_arr, t1map.affine, t1map.header)
-    t1map.to_filename(os.path.join(out_dir, 'sub-02_ses-01_run-01_T1map.nii.gz'))
-    mp2rage.t1w_uni.to_filename(os.path.join(out_dir, 'sub-02_ses-01_run-01_T1w.nii.gz'))
+    shutil.copyfile(fwd_transform, fwd_transform_filename)
 
-    # Correct for B1+ inhomogeneity
-    mp2rage.correct_for_B1(b1map_rescaled_reg_path)
-    t1map = mp2rage.t1map_b1_corrected
-    t1map_arr = t1map.get_fdata()
-    t1map_arr = t1map_arr / 1000  # Convert from milliseconds to seconds
-    t1map = nb.Nifti1Image(t1map_arr, t1map.affine, t1map.header)
-    t1map.to_filename(os.path.join(out_dir, 'sub-02_ses-01_run-01_desc-B1corrected_T1map.nii.gz'))
-    mp2rage.t1w_uni_b1_corrected.to_filename(
-        os.path.join(out_dir, 'sub-02_ses-01_run-01_desc-B1corrected_T1w.nii.gz')
-    )
+    # Apply transforms to MNI152NLin2009cAsym
+    ...
 
 
 if __name__ == '__main__':
     code_dir = '/Users/taylor/Documents/linc/nibs'
     in_dir = '/Users/taylor/Documents/datasets/nibs/dset'
-    out_dir = '/Users/taylor/Documents/datasets/nibs/derivatives/pymp2rage'
+    smriprep_dir = '/Users/taylor/Documents/datasets/nibs/derivatives/smriprep'
+    out_dir = '/Users/taylor/Documents/datasets/nibs/derivatives/mese'
     os.makedirs(out_dir, exist_ok=True)
-    temp_dir = '/Users/taylor/Documents/datasets/nibs/derivatives/pymp2rage/temp'
+    temp_dir = '/Users/taylor/Documents/datasets/nibs/work/mese'
     os.makedirs(temp_dir, exist_ok=True)
 
     dataset_description = {
-        'Name': 'NIBS MP2RAGE Derivatives',
+        'Name': 'NIBS MESE Derivatives',
         'BIDSVersion': '1.10.0',
         'DatasetType': 'derivative',
         'DatasetLinks': {
             'raw': in_dir,
+            'smriprep': smriprep_dir,
         },
         'GeneratedBy': [
             {
                 'Name': 'Custom code',
-                'Description': 'Custom Python code combining ANTsPy and pymp2rage.',
+                'Description': 'Custom Python code combining ANTsPy and tedana.',
                 'CodeURL': 'https://github.com/PennLINC/nibs',
             }
         ],
@@ -200,14 +309,33 @@ if __name__ == '__main__':
     with open(os.path.join(out_dir, 'dataset_description.json'), 'w') as f:
         json.dump(dataset_description, f, sort_keys=True, indent=4)
 
-    layout = BIDSLayout(in_dir, config=os.path.join(code_dir, 'nibs_bids_config.json'))
-    subjects = layout.get_subjects(suffix='MP2RAGE')
+    layout = BIDSLayout(
+        in_dir,
+        config=os.path.join(code_dir, 'nibs_bids_config.json'),
+        validate=False,
+        derivatives=[smriprep_dir],
+    )
+    subjects = layout.get_subjects(suffix='MESE')
     for subject in subjects:
         print(f'Processing subject {subject}')
-        sessions = layout.get_sessions(subject=subject, suffix='MP2RAGE')
+        sessions = layout.get_sessions(subject=subject, suffix='MESE')
         for session in sessions:
             print(f'Processing session {session}')
-            run_data = collect_run_data(layout, {'subject': subject, 'session': session})
-            process_run(layout, run_data, out_dir, temp_dir)
+            mese_files = layout.get(
+                subject=subject,
+                session=session,
+                echo=1,
+                part='mag',
+                direction='AP',
+                suffix='MESE',
+                extension=['.nii', '.nii.gz'],
+            )
+            for mese_file in mese_files:
+                entities = mese_file.get_entities()
+                entities.pop('echo')
+                entities.pop('part')
+                entities.pop('direction')
+                run_data = collect_run_data(layout, entities)
+                process_run(layout, run_data, out_dir, temp_dir)
 
     print('DONE!')
