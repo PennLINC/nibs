55eb209b36642dea64ee4117f17e6ca68bd80f12
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-09-03 12:59:09 -0400

    Create process_qsm_chisep.sh

---
+++ processing/process_qsm_chisep.sh
@@ -0,0 +1,9 @@
+# Submit an LSF array job to run process_qsm_prep.py for all subjects.
+# Submit with bsub -J "qsm_chisep[1-10]" process_qsm_chisep.sh
+
+subjects="24037 60501 60505 60514 60516 60518 60519 60520 60522 60526"
+
+# Select one subject based on the index
+subject=${subjects[$LSB_JOBINDEX]}
+
+python process_qsm_chisep.py --subject-id ${subject}
