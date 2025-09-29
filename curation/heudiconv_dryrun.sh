8152891152fcb595ff83488636b86d97f7184a52
tsalo <salot@pennmedicine.upenn.edu>
2025-09-29 10:40:50 -0400

    Remove unused files.

--- curation/heudiconv_dryrun.sh
+++
@@ -1,9 +0,0 @@
-heudiconv \
-    -d "/Users/taylor/Downloads/flywheel/bbl/NIBS_857664/techdev_human_myelin_{subject}/*/*/*/*.dcm" \
-    -o /Users/taylor/Downloads/flywheel/bbl/dset \
-    -f heuristic.py \
-    -s "02" \
-    --ses "01" \
-    -g all \
-    -c dcm2niix \
-    --bids
